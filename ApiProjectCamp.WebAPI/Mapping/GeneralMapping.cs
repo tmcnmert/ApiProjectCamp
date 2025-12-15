29f74fb7208ce77a508877a41a8b17b7e8ff85ae
tmcnmert <tmcnmert@gmail.com>
2025-12-15 23:09:29 +0300

    Add DTOs, ContactsController, and AutoMapper configuration
    - Created DTOs for Contact, Feature, and Message entities
    - Implemented CRUD operations in ContactsController using EF Core
    - Added AutoMapper mappings for entity-DTO transformations

---
+++ ApiProjectCamp.WebAPI/Mapping/GeneralMapping.cs
@@ -0,0 +1,24 @@
+﻿using ApiProjectCamp.WebAPI.Dtos.FeatureDtos;
+using ApiProjectCamp.WebAPI.Entities;
+using AutoMapper;
+
+namespace ApiProjectCamp.WebAPI.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,ResultFeatureDto>().ReverseMap();
+            CreateMap<Feature,CreateFeatureDto>().ReverseMap();
+            CreateMap<Feature,UpdateFeaturedTO>().ReverseMap();
+            CreateMap<Feature, GetByIdFeatureDto>().ReverseMap();
+
+
+            CreateMap<Message, ResultFeatureDto>().ReverseMap();
+            CreateMap<Message, CreateFeatureDto>().ReverseMap();
+            CreateMap<Message, UpdateFeaturedTO>().ReverseMap();
+            CreateMap<Message, GetByIdFeatureDto>().ReverseMap();
+            //AutoMapper'deki ReverseMap() metodu, iki yönlü dönüşüm sağlar. Yani, bir nesneyi diğerine dönüştürdüğünüz gibi, tersini de otomatik olarak yapmanıza olanak tanır.
+        }
+    }
+}
